b920f33c95561b13d175d6407cfeb2a353ae019b
Rouneq <rouneq@systemplayer.net>
2022-12-12 17:17:40 -0500

    Update version number

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 --Tutorial.lua by Rouneq
 --Based on Tutorial.mac by Chatwiththisname and Cannonballdex
---v0.99.1
+--v0.99.2
 --
 --Purpose:  Will conduct the tutorial for the current character
 --			from immediately after character creation
